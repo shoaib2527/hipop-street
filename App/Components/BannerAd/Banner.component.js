c5f31e4c29fc443eb7c6abb63fbcb26508f03302
Shoaib Ahmed <shoaib.ahmed47564@gmail.com>
2020-09-15 10:27:42 +0500

    Update Banner.component.js

--- App/Components/BannerAd/Banner.component.js
+++ App/Components/BannerAd/Banner.component.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import firebase from 'react-native-firebase';
-import {Platform} from 'react-native'
+import { Platform } from 'react-native'
 const Banner = firebase.admob.Banner;
 const AdRequest = firebase.admob.AdRequest;
 const request = new AdRequest();
 request.addKeyword('foobar');
 const BANNERID = Platform.OS == 'ios' ? "ca-app-pub-5865232026493005/6091416462"
-: "ca-app-pub-5865232026493005/7806811057";
+    : "ca-app-pub-5865232026493005/7806811057";
 export default function BannerComponent() {
     return (
         <Banner
